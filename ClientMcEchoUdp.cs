e3e99c4f2cce8554df04474ef2dda7d61d5f4695
Vincent Van Assche <vincent.vanassche@unicaen.fr>
2015-03-31 15:22:26 +0200

    Archivage initial du module UdpMcEchoClient

---
+++ ClientMcEchoUdp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+namespace UdpMcEchoClient
+{
+	public class ClientMcEchoUdp
+	{
+		IPEndPoint mcEndPoint;
+		UdpClient client = null;
+
+		public ClientMcEchoUdp (IPEndPoint mcEndPoint)
+		{
+			byte prefix = mcEndPoint.Address.GetAddressBytes ()[0];
+			prefix &= 240;
+			if (prefix != 224)//if (addressBytes [0] < 224 | addressBytes [0] > 239)
+				throw new Exception ("Adresse multicast doit avoir le préfixe 224.0.0.0/4 !");
+			this.mcEndPoint = mcEndPoint;
+
+			client = new UdpClient (mcEndPoint.Port);
+			client.JoinMulticastGroup (mcEndPoint.Address);
+			//client.Client.SetSocketOption(SocketOptionLevel.IP,SocketOptionName.AddMembership,new MulticastOption(mcEndPoint.Address,mcEndPoint.Port));
+			Console.WriteLine ("Client abonné à {0}", mcEndPoint);
+		}
+
+		public void Envoyer(string msg, IPEndPoint serveur)
+		{
+			IPEndPoint epRcv = new IPEndPoint (IPAddress.Any, 0);
+			byte[] tabEnvoi = UTF8Encoding.UTF8.GetBytes (msg);
+			client.Send (tabEnvoi,tabEnvoi.Length,serveur);
+			Console.WriteLine ("Envoyé \"" + msg+"\" à "+serveur.ToString());
+
+			byte[] tabRcv = client.Receive (ref epRcv);
+			Console.WriteLine("Reçu \""+ UTF8Encoding.UTF8.GetString(tabRcv)+"\" de "+epRcv.ToString());
+
+			//client.Client.SetSocketOption(SocketOptionLevel.IP,SocketOptionName.DropMembership,new MulticastOption(mcEndPoint.Address,mcEndPoint.Port));
+			client.DropMulticastGroup (mcEndPoint.Address);
+		}
+
+
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace UdpMcEchoClient
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			IPEndPoint mcEp = new IPEndPoint(IPAddress.Parse("224.0.0.1"),1338);
+			ClientMcEchoUdp echoClient = new ClientMcEchoUdp(mcEp);
+			Console.WriteLine ("Client crée…");
+
+			IPEndPoint serveur = new IPEndPoint(IPAddress.Parse("10.42.43.2"),1337);
+			echoClient.Envoyer ("Ceci est un nouveau message.", serveur);
+		}
+	}
+}
